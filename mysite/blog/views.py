a204ce398d7e8b9fd9e82bd7cce9f87ef1f58b83
mehdi <mehdip.m909@gmail.com>
2024-05-08 07:34:33 +0330

    views and templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts':posts})
+
+def post_detail(request, id):
+    status = Post.Status.PUBLISHED
+    post = get_object_or_404(Post,id=id,status=status)
+    template = 'blog/post/detail.html'
+    return render(request, template, {'post': post})
