cdf5d6847970873b3842beb16bc07a6d281775ce
mehdi <mehdip.m909@gmail.com>
2024-06-05 22:04:17 +0330

    optimize searching feature

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,7 +8,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -111,12 +111,14 @@ def post_search(request):
     results = []
 
     if 'query' in request.GET:
-        form = SearchForm(request.Get)
+        form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector, rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
 
     return render(request,
                   'blog/post/search.html',
